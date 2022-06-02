21000e9f85dd897ff27e42ff7e048d7281d789f4
yuito1227 <yuito3226@gmail.com>
2022-06-02 23:51:03 +0900

    add_scraping

--- Earthquake/Earthquake.go
+++ Earthquake/Earthquake.go
@@ -1,41 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/saintfish/chardet"
-	"golang.org/x/net/html/charset"
 )
 
 func main() {
-	url := "https://fortune.yahoo.co.jp/12astro/20190922/scorpio.html"
-
-	// Getリクエスト
-	res, _ := http.Get(url)
-	defer res.Body.Close()
-
-	// 読み取り
-	buf, _ := ioutil.ReadAll(res.Body)
-
-	// 文字コード判定
-	det := chardet.NewTextDetector()
-	detRslt, _ := det.DetectBest(buf)
-	fmt.Println(detRslt.Charset)
-	// => EUC-JP
-
-	// 文字コード変換
-	bReader := bytes.NewReader(buf)
-	reader, _ := charset.NewReaderLabel(detRslt.Charset, bReader)
-
-	// HTMLパース
-	doc, _ := goquery.NewDocumentFromReader(reader)
-
-	// titleを抜き出し
-	rslt := doc.Find("title").Text()
-	fmt.Println(rslt)
-	// => さそり座(蠍座) 今日の運勢 - Yahoo!占い
+	doc, err := goquery.NewDocument("https://www.jma.go.jp/bosai/forecast/#area_type=offices&area_code=270000")
+	if err != nil {
+		panic("htmlの取得に失敗しました")
+	}
+	title := doc.Find(".forecast-sentence")
+	fmt.Print(title)
+	title.Each(func(i int, s *goquery.Selection) {
+		fmt.Print(s.Text())
+	})
 }
--- Earthquake/go.mod
+++ Earthquake/go.mod
@@ -1,11 +1,9 @@
-module example.com/m/v2
+module main_mod
 
 go 1.18
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
 )
